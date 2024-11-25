2783bcf77f7cc680ee6381dd3f6d3105f7a033f3
Tejasvi-Garg <gargtejasvi076@gmail.com>
2024-11-25 20:36:19 +0530

    first

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// import React from 'react'
+import News from './Component/News'
+
+
+const App = () => {
+  return (
+    <div className='container'>
+      <div className='news-app'>
+      <News/>
+    </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Component/Calendar.jsx
@@ -0,0 +1,8 @@
+import './Calendar.css'
+const Calendar = () => {
+  return (
+    <div className='calendar'>Calendar</div>
+  )
+}
+
+export default Calendar
